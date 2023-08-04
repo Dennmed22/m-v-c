47f4ca6af7984e31a8431665c7f41915c4f9762c
Dennmed22 <medrano.dennis@yahoo.com>
2023-08-04 17:02:02 -0400

    added model index.js files

---
+++ Main/models/index.js
@@ -0,0 +1,54 @@
+const { DataTypes, Model } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+  {
+    // Define user model properties here
+  },
+  {
+    sequelize,
+    modelName: 'user',
+  }
+);
+
+class Record extends Model {}
+
+Record.init(
+  {
+    // Define record model properties here
+  },
+  {
+    sequelize,
+    modelName: 'record',
+  }
+);
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    // Define comment model properties here
+  },
+  {
+    sequelize,
+    modelName: 'comment',
+  }
+);
+
+Record.belongsTo(User, {
+  foreignKey: 'user_id',
+});
+
+Record.hasMany(Comment, {
+  foreignKey: 'record_id',
+  onDelete: 'CASCADE',
+});
+
+User.hasMany(Record, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE',
+});
+
+module.exports = { User, Comment, Record };
